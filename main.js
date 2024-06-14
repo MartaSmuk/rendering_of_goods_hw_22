06c2a4b389f7253fdac724a96898f14ce7ad5fc9
Marta Smuk <msmuk999@gmail.com>
2024-06-15 00:15:12 +0200

    added first task

---
+++ main.js
@@ -0,0 +1,80 @@
+"use strict";
+let kitchenProducts = [
+	{
+		type: 'grater',
+		price: 10
+	},
+	{
+		type: 'pastry-bag',
+		price: 25
+	},
+	{
+		type: 'scale',
+		price: 5
+	},
+	{
+		type: 'whisk',
+		price: 15
+	}
+];
+
+let devicesProducts = [
+	{
+		type: 'desktop',
+		price: [100,1000]
+	},
+	{
+		type: 'laptop',
+		price: [50,1500]
+	},
+	{
+		type: 'smartphone',
+		price: [80,2000]
+	},
+	{
+		type: 'tablet',
+		price: [20,1300]
+	}
+];
+
+let cosmeticsProducts = [
+	{
+		type: 'blush',
+		price: 100
+	},
+	{
+		type: 'eyeshadow',
+		price: 50
+	},
+	{
+		type: 'lipstick',
+		price: 80
+	},
+	{
+		type: 'nail-polish',
+		price: 200
+	},
+	{
+		type: 'perfume',
+		price: 300,
+	}
+];
+
+// 1. Для кожної категорії створити об'єкт, який має лише одну властивість 
+// category, де буде ім'я категорії. Наприклад, let Kitchen = {category: 'kitchen'};
+
+let Kitchen = {
+    category: "kitchen",
+}
+
+let Devices = {
+    category: "devices",
+}
+
+let Cosmetics = {
+    category: "cosmetics",
+}
+
+//Всім товарам з массивів kitchenProducts, devicesProducts, cosmeticsProducts
+//добавити в якості об'єкта-прототипа об'єкти, ствоорені на першому кроці. 
+//Після цього, доповнити кожен товар своїми властивостями.
